eefc6b23e6def8d9a354d112fb0bc68c716fcaca
programadorbt7 <programadorbt7@gmail.com>
2024-07-20 22:16:07 -0500

    Inicializando proyecto angular, prueba de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { PruebaComponentComponent } from './prueba-component/prueba-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PruebaComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'signal-store';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+  
 }
---
+++ src/app/prueba-component/prueba-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './prueba-component.component.html',
+  styleUrl: './prueba-component.component.css'
+})
+export class PruebaComponentComponent {
+
+}
